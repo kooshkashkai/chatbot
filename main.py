a536311c22a99ec1e4be15a73ee1b929eca78ed9
kooshkashkai <dgwalters@me.com>
2024-01-05 14:10:54 +0000

    SimpleWeb reader - only use locally

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 from llama_index import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 import openai
-#from llama_index import SimpleDirectoryReader
-from llama_index import download_loader
+from llama_index import SimpleDirectoryReader
+from llama_index.readers import download_loader
 import os
 import requests
 from bs4 import BeautifulSoup
@@ -12,6 +12,8 @@ import url_data
 
 openai.api_key = st.secrets.openai_key
 
+
+# Code used for generating sub Url's:
 # # Get sub url's from website:
 # url = 'https://solumxplain.github.io/docs.xplainfinancial/'
 # reqs = requests.get(url)
@@ -20,13 +22,13 @@ openai.api_key = st.secrets.openai_key
 # urls = []
 # for link in soup.find_all('a'):
 #     urls.append(link.get('href'))
-# urls = ['https://solumxplain.github.io' + s for s in urls if s[0:1] != "#"]
+# urls = ['https://solumxplain.github.io/docs.xplainfinancial' + s for s in urls if s[0:1] != "#"]
 
 #urls=['https://solumxplain.github.io/docs.xplainfinancial/']
 
 st.header("Xplain docs interactive chatbot 💬 🧐 🐶")
 
-if "messages" not in st.session_state.keys(): # Initialize the chat message history
+if "messages" not in st.session_state.keys():
     st.session_state.messages = [
         {"role": "assistant", "content": "Ask me a question about Xplain!"}
     ]
@@ -35,9 +37,11 @@ if "messages" not in st.session_state.keys(): # Initialize the chat message hist
 
 def load_data():
     with st.spinner(text="Loading and indexing the Xplain docs..."):
-        SimpleWebPageReader = download_loader("SimpleWebPageReader")
+        SimpleWebPageReader = download_loader("SimpleWebPageReader", custom_path=".cache/llamahub_modules")
         loader = SimpleWebPageReader()
         docs = loader.load_data(urls=url_data.url_data)
+        #reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+        #docs = reader.load_data()
         service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Xplain docs and your job is to answer technical questions. Assume that all questions are related to the Xplain docs. Give full details in your answers rather than referring to the Xplain docs. Keep your answers technical and based on facts – do not hallucinate features."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
